e0e0edfcf3f75f7582f3215b472e77e0a4b30045
Roberto Cinetto <roberto@MacBook-Pro.homenet.telecomitalia.it>
2016-02-22 23:21:02 +0100

    mailer settings production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,16 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: 'cynaz-saas-guide.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'cynaz-saas-guide.herokuapp.com',
+    user_name:            ENV["gmail_username"],
+    password:             ENV["gmail_password"],
+    authentication:       'plain',
+    enable_starttls_auto: true  
+  }
 
   config.logger = Logger.new(STDOUT)
 end
