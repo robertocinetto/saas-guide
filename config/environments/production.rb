429b9a3186f52df80f4b471d95f80e6e84603eb6
Roberto Cinetto <roberto@MacBook-Pro.homenet.telecomitalia.it>
2016-02-22 23:08:53 +0100

    logger settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: 'cynaz-saas-guide.herokuapp.com' }
+
+  config.logger = Logger.new(STDOUT)
 end
