755f7013dfaea7ddac9ab69b0e8302901058edfd
Roberto Cinetto <roberto@MacBook-Pro.homenet.telecomitalia.it>
2016-02-22 20:39:48 +0100

    email in creazione account fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,7 @@ class User < ActiveRecord::Base
   private
   #email should be unique in Account model
   def email_is_unique
+    #do not validate email if errors are present
     return false unless self.errors[:email].empty?
     unless Account.find_by_email(:email).nil?
       errors.add :email, " is already used"
@@ -22,6 +23,6 @@ class User < ActiveRecord::Base
   end
 
   def create_account
-    account = Account.create email: :email
+    account = Account.create email: email
   end
 end
